6dbf7f96c4ce7919ef5549a969e7553adb6abc8d
Kenton Hamaluik <hamaluik@ualberta.ca>
2016-06-20 13:38:15 -0600

    feat: app launches to notification icon rather than main window

--- Scriba/App.xaml.cs
+++ Scriba/App.xaml.cs
@@ -11,5 +11,53 @@ namespace Scriba {
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application {
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private bool isExit = false;
+
+		protected override void OnStartup(StartupEventArgs e) {
+			base.OnStartup(e);
+
+			MainWindow = new MainWindow();
+			MainWindow.Closing += MainWindow_Closing;
+
+			notifyIcon = new System.Windows.Forms.NotifyIcon();
+			notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
+			notifyIcon.Icon = Scriba.Properties.Resources.scriba;
+			notifyIcon.Visible = true;
+
+			CreateContextMenu();
+		}
+
+		private void CreateContextMenu() {
+			notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+			notifyIcon.ContextMenuStrip.Items.Add("Show").Click += (s, e) => ShowMainWindow();
+			notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+		}
+
+		private void ShowMainWindow() {
+			if(MainWindow.IsVisible) {
+				if(MainWindow.WindowState == WindowState.Minimized) {
+					MainWindow.WindowState = WindowState.Normal;
+				}
+				MainWindow.Activate();
+			}
+			else {
+				MainWindow.Show();
+			}
+		}
+
+		private void ExitApplication() {
+			isExit = true;
+			MainWindow.Close();
+			notifyIcon.Dispose();
+			notifyIcon = null;
+		}
+
+		private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+			if(!isExit) {
+				e.Cancel = true;
+				MainWindow.Hide();
+			}
+		}
 	}
 }
